1512d7b3d1057ac07f3f05cef4457d10630c6389
William John Holden <wjholden@gmail.com>
2020-12-31 19:39:46 +0100

    IPv6 support

---
+++ Backup-Borderlands3.ps1
@@ -0,0 +1,10 @@
+﻿function Backup-Borderlands3() {
+    $d = Get-Date -Format "yyyy-MM-dd"
+    Set-Location "C:\Users\wjhol\Google Drive\BL3"
+    New-Item -ItemType Directory -Name $d -ErrorAction SilentlyContinue
+    $dst = Get-Item $d
+    Copy-Item -Path 'C:\Users\wjhol\Documents\My Games\Borderlands 3\Saved\SaveGames\*\*' -Destination $dst -Recurse
+    return $dst
+}
+
+New-Alias -Name bb -Value Backup-Borderlands3
--- Syslog.psm1
+++ Syslog.psm1
@@ -1,17 +1,27 @@
 ﻿function Send-Syslog {
     Param(
-        [Parameter(Mandatory=$true, ParameterSetName="IP Address")]
+        [Parameter(Mandatory=$true, ParameterSetName="IP Address", Position=0)]
         [string]$IPAddress,
 
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$true, ParameterSetName="IPv6 Address", Position=0)]
+        [string]$IPv6Address,
+
+        [Parameter(Mandatory=$true, Position=1)]
         [string]$Message
     )
 
-    $ep = New-Object IPEndPoint (
-        [IPAddress]::Parse($IPAddress), 514);
+    $af = -1;
+    if ($IPAddress) {
+        $af = [System.Net.Sockets.AddressFamily]::InterNetwork;
+        $ep = New-Object IPEndPoint ([IPAddress]::Parse($IPAddress), 514);
+    } else {
+        $af = [System.Net.Sockets.AddressFamily]::InterNetworkV6;
+        $ep = New-Object IPEndPoint ([IPAddress]::Parse($IPv6Address), 514);
+    }
+
     try
     {
-        $socket = New-Object System.Net.Sockets.UdpClient;
+        $socket = [System.Net.Sockets.UdpClient]::new($af);
         $socket.Connect($ep);
         $bytes = [System.Text.Encoding]::ASCII.GetBytes($message);
         $i = $socket.Send($bytes, $bytes.length);
@@ -28,22 +38,33 @@
 
 function Receive-Syslog {
     Param(
-        [Parameter(Mandatory=$true, ParameterSetName="IP Address")]
-        [string]$IPAddress
+        [Parameter(Mandatory=$true, ParameterSetName="IP Address", Position=0)]
+        [string]$IPAddress,
+
+        [Parameter(Mandatory=$true, ParameterSetName="IPv6 Address", Position=0)]
+        [string]$IPv6Address
     )
+
+    if ($IPAddress) {
+        $addr = [IPAddress]::Parse($IPAddress);
+        $af = [System.Net.Sockets.AddressFamily]::InterNetwork;
+    } else {
+        $addr = [IPAddress]::Parse($IPv6Address);
+        $af = [System.Net.Sockets.AddressFamily]::InterNetworkV6;
+    }
 
     $ep = New-Object IPEndPoint ([IPAddress]::Any, 0);
 
     try
     {
-        $socket = New-Object System.Net.Sockets.UdpClient 514;
-        $socket.JoinMulticastGroup(([IPAddress]::Parse($IPAddress)));
+        $socket = New-Object System.Net.Sockets.UdpClient @(514, $af);
+        $socket.JoinMulticastGroup($addr);
         $bytes = $socket.Receive([ref] $ep);
         [System.Text.Encoding]::ASCII.GetString($bytes);
     }
     catch [System.Net.Sockets.SocketException]
     {
-        Write-Error "Unable to receive Syslog messages on $IPAddress";
+        Write-Error "Unable to receive Syslog messages on $addr";
     }
     finally
     {
